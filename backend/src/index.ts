abd9a0e3bd22e6e479febae4a28c22d2d3df19f5
Edwar Sanchez <edwar.s98@hotmail.com>
2025-05-03 06:10:28 -0500

    chore: crear estructura de directorios para backend y frontend, crear configuracion de de docker

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI || "";
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("Conexión exitosa a MongoDB"))
+  .catch((err) => console.error("Error al conectar con MongoDB:", err));
+
+app.get("/", (_req, res) => {
+  res.send("TaskFlow Backend funcionando");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor backend corriendo en http://localhost:${PORT}`);
+});
